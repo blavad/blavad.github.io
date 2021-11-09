1e263aef2f3dad7bda01b43afd9b9c1b3c9fdea3
blavad <david.albert@insa-lyon.fr>
2021-11-09 01:16:43 +0100

    Update research section

--- src/components/sections/ResearchSection.vue
+++ src/components/sections/ResearchSection.vue
@@ -1,28 +1,32 @@
 <template>
   <!-- Projects -->
   <div id="research" class="container-fluid text-light parallax">
-    <!-- <SectionTitle /> -->
-    <SectionTitle :title="lang == 'us' ? 'Research Interests' : 'Recherche'" />
-    <p id="description">
-      My main research interest is about the resolution of collaborative
-      multi-agent problems with RL methods.
-    </p>
+    <div class="container text-light parallax">
+      <!-- <SectionTitle /> -->
+      <SectionTitle
+        :title="lang == 'us' ? 'Research Interests' : 'Recherche'"
+      />
+      <p id="description">
+        My main research interest is about the resolution of collaborative
+        multi-agent problems with RL methods.
+      </p>
 
-    <Publication
-      title="SDMS : An open-source library for POSGs"
-      authors="D.Albert & J. Dibangoye"
-      date="2021"
-      url=""
-      abstract="A solver and a library for POSGs."
-    />
+      <Publication
+        title="SDMS : An open-source library for POSGs"
+        authors="D.Albert & J. Dibangoye"
+        date="2021"
+        url=""
+        abstract="A solver and a library for POSGs."
+      />
 
-    <Publication
-      title="On continuous-state MDPs in extensive-form"
-      authors="J. Arjonilla, D. Albert, J. Dibangoye"
-      date="Delayed"
-      abstract="This paper presents a novel—yet
+      <Publication
+        title="On continuous-state MDPs in extensive-form"
+        authors="J. Arjonilla, D. Albert, J. Dibangoye"
+        date="Delayed"
+        abstract="This paper presents a novel—yet
 more scalable—alternative, namely serial central planning for simultaneous decentralised execution."
-    />
+      />
+    </div>
   </div>
 </template>
 
@@ -32,7 +36,7 @@ import SectionTitle from "@/components/basic/SectionTitle.vue";
 import Publication from "@/components/Publication.vue";
 
 export default {
-  name: "ProjectSection",
+  name: "ResearchSection",
   props: {
     publications: Object,
   },
@@ -56,7 +60,7 @@ export default {
 }
 
 #description {
+  margin: 0 2.5rem;
   font-weight: bolder;
-  text-align: center;
 }
 </style>
