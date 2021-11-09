cb0e222c61d4a5273cbbec9c89a94f7f78158b81
blavad <david.albert@insa-lyon.fr>
2021-11-09 19:48:21 +0100

    Test5

--- src/components/sections/ResearchSection.vue
+++ src/components/sections/ResearchSection.vue
@@ -11,12 +11,22 @@
         multi-agent problems with RL methods.
       </p>
 
-      <div class="publication px-4 py-2 bg-light text-dark">
-        <div class="publication-title">Test</div>,
+      <div class="publi px-4 py-2 bg-light text-dark">
+        <div class="publi-title">Test</div>,
         <div class="pauthors">David</div>,
         <div class="pdate">[2021]</div>
         <p class="pabstract">blablabla</p>
       </div>
+<!-- 
+      <Publication
+        v-for="p in publications"
+        :key="p.title"
+        :title="p.title"
+        :authors="p.authors"
+        :date="p.date"
+        :url="p.url"
+        :abstract="p.abstract"
+      /> -->
     </div>
   </div>
 </template>
@@ -59,4 +69,38 @@ export default {
   background-repeat: no-repeat;
 }
 
+.publi {
+  margin: 2rem 0rem;
+  opacity: 76%;
+  border-radius: 4px;
+}
+
+.publi-title {
+  display: inline;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #0e76a8;
+}
+
+.publi-title a {
+  font-weight: bold;
+  color: #0e76a8;
+}
+
+.pauthors {
+  display: inline;
+  font-size: 1rem;
+  font-style: italic;
+  font-weight: bolder;
+}
+
+.pdate {
+  display: inline;
+  font-size: 1rem;
+  font-weight: bolder;
+}
+
+.pabstract {
+  font-size: 1rem;
+}
 </style>
