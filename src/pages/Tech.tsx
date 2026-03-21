2f98812cc3d2091342af1875f3db1a0ab3843646
blavad <david.albert@cegetel.net>
2026-03-21 18:36:47 +0100

    update project

--- src/pages/Tech.tsx
+++ src/pages/Tech.tsx
@@ -61,11 +61,11 @@ function Tech() {
             </section>
             <section id="projects" className="mt-20 w-full px-5 lg:px-40">
                 <h3>Quelques projets</h3>
-                <div className="mt-15 grid w-full grid-cols-2 gap-4 lg:grid-cols-4">
+                <div className="mt-15 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
                     {Projects.map((project) => (
                         <div
                             key={project.short_title}
-                            className="group relative aspect-4/3 cursor-pointer overflow-hidden rounded-2xl"
+                            className="group relative aspect-8/3 cursor-pointer overflow-hidden rounded-2xl sm:aspect-4/3"
                             onClick={() =>
                                 window.open(project.url, '_blank', 'noopener noreferrer')
                             }
@@ -76,8 +76,8 @@ function Tech() {
                                 className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
                             />
                             <div className="absolute inset-0 bg-linear-to-t from-black/75 via-black/10 to-transparent" />
-                            <div className="absolute right-0 bottom-0 left-0 translate-y-9 p-3 transition-transform duration-300 group-hover:translate-y-0">
-                                <p className="text-sm font-bold leading-tight text-white">
+                            <div className="absolute right-0 -bottom-1 left-0 translate-y-9 p-3 transition-transform duration-300 group-hover:translate-y-0">
+                                <p className="text-sm leading-tight font-bold text-white">
                                     {project.short_title}
                                 </p>
                                 <p className="mt-1 line-clamp-2 text-xs text-white/75">
