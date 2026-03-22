47dfbf56280ae743167a132c1697cb2c8019829e
blavad <david.albert@cegetel.net>
2026-03-22 12:16:21 +0100

    setup translation mode

--- src/config/translations/translations.tsx
+++ src/config/translations/translations.tsx
@@ -1,4 +1,22 @@
 import en from './en.json';
 import fr from './fr.json';
 
-export { en, fr };
+export const TRANSLATIONS = {
+    en: {
+        translation: en,
+    },
+    fr: {
+        translation: fr,
+    },
+    // es: {
+    //     translation: es,
+    // },
+    // de: {
+    //     translation: de,
+    // },
+    // zh: {
+    // 	translation: zh,
+    // },
+};
+
+export const DEFAULT_LANGUAGE = 'fr';
