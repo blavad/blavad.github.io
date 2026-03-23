4e470de0c940fff25b454dace0b73a65a364e92a
blavad <david.albert@cegetel.net>
2026-03-23 18:55:10 +0100

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import viteCompression from 'vite-plugin-compression';
-import vitePrerender from 'vite-plugin-prerender';
-
-const PuppeteerRenderer = vitePrerender.PuppeteerRenderer;
-const distDir = fileURLToPath(new URL('./dist', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,14 +14,6 @@ export default defineConfig({
         tailwindcss(),
         viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
         viteCompression({ algorithm: 'gzip', ext: '.gz' }),
-        vitePrerender({
-            staticDir: distDir,
-            routes: ['/', '/tech', '/teaching'],
-            renderer: new PuppeteerRenderer({
-                args: ['--no-sandbox', '--disable-setuid-sandbox'],
-                renderAfterTime: 2000,
-            }),
-        }),
     ],
     resolve: {
         alias: [
